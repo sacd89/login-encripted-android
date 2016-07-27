d0420cd94b6ce8f7ebb07056c2d9c1add273dbf8
eZubia <edzh18@gmail.com>
2016-07-26 18:41:55 -0600

    Se repara iBeacon.

--- ProyectoSeguridadRedes/app/src/main/java/seguridad_redes/uach/mx/proyectoseguridadredes/TabsActivity.java
+++ ProyectoSeguridadRedes/app/src/main/java/seguridad_redes/uach/mx/proyectoseguridadredes/TabsActivity.java
@@ -3,29 +3,24 @@ package seguridad_redes.uach.mx.proyectoseguridadredes;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.AsyncTask;
-import android.support.design.widget.TabLayout;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bluebite.android.eddystone.Global;
@@ -65,15 +60,13 @@ public class TabsActivity extends AppCompatActivity implements ScannerDelegate {
     private BeaconAdapter mBeaconAdapter;
     private static final String TAG = MainActivity.class.getSimpleName();
     private List<Url> mUrls = new ArrayList<>();
-    private ListView lstVwUsuarios;
     public static String URL_PENDIENTE;
-    List<Pendiente> pendientes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tabs);
-        System.out.println("mBluetoothAdapter = " + mBluetoothAdapter);
+        //System.out.println("mBluetoothAdapter = " + mBluetoothAdapter);
         if (!mBluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
@@ -118,7 +111,7 @@ public class TabsActivity extends AppCompatActivity implements ScannerDelegate {
         System.out.println("usuario = " + usuario);
         getSupportActionBar().setTitle(usuario);
 
-        items = getPendientes();
+        //items = getPendientes();
         if(items == null){
             Toast.makeText(this,"No tienes tareas pendientes", Toast.LENGTH_LONG);
         }else {
@@ -150,8 +143,9 @@ public class TabsActivity extends AppCompatActivity implements ScannerDelegate {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                mBeaconAdapter.clear();
-                mBeaconAdapter.addAll(mUrls);
+                //mBeaconAdapter.clear();
+                System.out.println("WE MADE IT ONE TIME");
+                //mBeaconAdapter.addAll(mUrls);
             }
         });
         
@@ -161,10 +155,10 @@ public class TabsActivity extends AppCompatActivity implements ScannerDelegate {
             System.out.println("str = " + str);
             if (str.equals("http://bit.ly/2a2QDMc") || true) {
                 URL_PENDIENTE = str;
-                pendientes = getPendientes();
-                ArrayAdapter<Pendiente> adapter = new ArrayAdapter<Pendiente>(this,
-                        android.R.layout.activity_list_item, android.R.id.text1, pendientes);
-                this.lstVwUsuarios.setAdapter(adapter);
+                items = getPendientes();
+                //ArrayAdapter<Pendiente> adapter = new ArrayAdapter<Pendiente>(this,
+                //        android.R.layout.activity_list_item, android.R.id.text1, pendientes);
+                //this.lstVwUsuarios.setAdapter(adapter);
             }
             System.out.println("URL_PENDIENTE After pedo = " + URL_PENDIENTE);
 
@@ -172,6 +166,7 @@ public class TabsActivity extends AppCompatActivity implements ScannerDelegate {
     }
 
     public List<Pendiente> getPendientes(){
+        System.out.println("MAMA MIA!!!!");
         ConnectServer server = new ConnectServer();
         server.execute();
         List<Pendiente> pendientes = new ArrayList<>();
